84054001803fb84367512502eb5caa1cd4cc85c6
Olajcodes <olajideabioye01@gmail.com>
2025-08-07 15:12:45 +0100

    Class practice

--- working_with_variable.py
+++ working_with_variable.py
@@ -8,7 +8,15 @@ My_Tax = 10000.50
 
 print(f"The ID for {First_Name} is {My_ID} and the amount of Tax to be paid is {My_Tax}")
 
-int()
-float()
-str()
-bool()
+# int()
+# float()
+# str()
+# bool()
+
+# Converting string to integer
+age = int(input("Enter your age: "))
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter second number: "))
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}")
