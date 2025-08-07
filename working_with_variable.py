3a1c72f00ec27a333f72357181e358b75aff6327
Olajcodes <olajideabioye01@gmail.com>
2025-08-07 14:24:07 +0100

    August 8 2025

---
+++ working_with_variable.py
@@ -0,0 +1,14 @@
+#   Working with Variables {String, Integer, Float}
+
+First_Name = "Olajcodes"
+
+My_ID = 20
+
+My_Tax = 10000.50
+
+print(f"The ID for {First_Name} is {My_ID} and the amount of Tax to be paid is {My_Tax}")
+
+int()
+float()
+str()
+bool()
