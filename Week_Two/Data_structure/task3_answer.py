c033da40f998e3ab65e703e9951bba57c349d4a2
Olajcodes <olajideabioye01@gmail.com>
2025-08-11 15:35:44 +0100

    Pending task to be completed at hom

---
+++ Week_Two/Data_structure/task3_answer.py
@@ -0,0 +1,29 @@
+# TASK 1
+
+# program to accept a string input from user and display it in uppercase
+string_input = input("Kindly provide your first name: ")
+print(string_input.upper())
+
+given_string = "python"
+print(given_string[0], given_string[-1])
+
+user_name = input("What\'s your name?: ")
+print("Hello,!", user_name)
+
+character = input("Enter a word: ")
+character_length = character.find(character[-1]) + 1
+print(character_length)
+
+given_string = "Hello World"
+print(given_string.replace("World","Python"))
+
+#   TASK 2
+
+given_string = input("Enter a word: ")
+print("python" in given_string)
+
+#  TASK 3
+# 12. Ask the user for a sentence and print each word on a new line
+user_input = input("Enter a sentence: ")
+user
+print(user_input.split())
