7ac5c50956895e5d56c494256e249bc7dec6f2c3
Olajcodes <olajideabioye01@gmail.com>
2025-08-19 15:39:41 +0100

    Adjusted few things

---
+++ Week_Two/Data_structure/14_08_2025/working_with_dictionary.py
@@ -0,0 +1,12 @@
+#   Creating Dictionaries
+# Using Curly Braces
+student = {
+    "name": "Olajide",
+    "age": 25,
+    "course": "Computer Science"
+}
+print(student)
+
+#  Using the dict() construcor
+student_info = dict(name = "John", age = 25, course = "Maths")
+print(student_info)
