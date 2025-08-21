cc84ef0ed9d5e0b8b4c66772812b78b6430500e9
Olajcodes <olajideabioye01@gmail.com>
2025-08-21 15:44:43 +0100

    Coding along

---
+++ Week_Three/21_08_2025/coding_along_error_handling.py
@@ -0,0 +1,10 @@
+#  Handling Errors in Python
+
+#  Syntax Errors
+#  Indenation Error - Incorrect spacing
+for i in range(3):
+print(i)
+
+#  Missing Colon/Parenthesis Error
+if 5 > 3
+print("Hello")
