a41f8c33c3f83a5750e1a09299ffb8cee607621b
Olajcodes <olajideabioye01@gmail.com>
2025-09-03 10:00:19 +0100

    Week five updating

--- Week_Three/18_08_2025/coding_alon_operator.py
+++ Week_Three/18_08_2025/coding_alon_operator.py
@@ -1,82 +1,82 @@
-# a = 10
-# b = 20
+a = 10
+b = 20
 
-# print(a ==b)
+print(a ==b)
 
-# print(a != b)
+print(a != b)
 
-# print(a > b)
+print(a > b)
 
-# print(a < b)
+print(a < b)
 
-# print(a >= 10)
+print(a >= 10)
 
-# print(b <= 25)
+print(b <= 25)
 
-# score = 75
-# print(score >= 50)
-# print(score < 50)
-# print(score == 100)
+score = 75
+print(score >= 50)
+print(score < 50)
+print(score == 100)
 
-# #  Assignment Operators
-# x = 10
-# print("Initial value:", x)
+#  Assignment Operators
+x = 10
+print("Initial value:", x)
 
-# x += 5
-# print("After x +=5:", x)
+x += 5
+print("After x +=5:", x)
 
-# x -= 2
-# print("After x -=2:", x)
+x -= 2
+print("After x -=2:", x)
 
-# x *= 3
-# print("After x *= 3:", x)
+x *= 3
+print("After x *= 3:", x)
 
-# x /= 4
-# print("After x/= 4:", x)
+x /= 4
+print("After x/= 4:", x)
 
-# x %= 3
-# print("After x %=3:", x)
+x %= 3
+print("After x %=3:", x)
 
-# x = 4
-# x **= 2
-# print("After x **= 2:", x)
+x = 4
+x **= 2
+print("After x **= 2:", x)
 
-# x //= 3
-# print("After x// 3:", x)
+x //= 3
+print("After x// 3:", x)
 
 #  Use case Example:
 #  define variables
-balance = 1000
-deposit = 200
-withdraw = 150
+# balance = 1000
+# deposit = 200
+# withdraw = 150
 
-balance += deposit
-balance -= withdraw 
-print("Updated Balance:", balance)
+# balance += deposit
+# balance -= withdraw 
+# print("Updated Balance:", balance)
 
-#  Logical Operators
-x = 10
-y = 20
+# #  Logical Operators
+# x = 10
+# y = 20
 
-print(x > 5 and y > 15)
+# print(x > 5 and y > 15)
 
-print(x < 5 or y > 15)
+# print(x < 5 or y > 15)
 
-print(not(x == 10))
+# print(not(x == 10))
 
-#  Use case scenario - Scholarship Eligibility
+# #  Use case scenario - Scholarship Eligibility
 
-age = 17
-score = 85
+# age = 17
+# score = 85
 
-eligible = (age < 18) and (score > 80)
+# eligible = (age < 18) and (score > 80)
 
-print("Scholarship Eligible:", eligible)
+# print("Scholarship Eligible:", eligible)
 
-# Another Use Scenario - Event Access
-age =  22
-has_ticket = False
+# # Another Use Scenario - Event Access
+# age =  22
+# has_ticket = False
 
-can_enter = (age >= 18) and (has_ticket or age < 25)
+# can_enter = (age >= 18) and (has_ticket or age < 25)
 
-print("Access Granted:", can_enter)
+# print("Access Granted:", can_enter)
--- reverse.py
+++ reverse.py
@@ -18,8 +18,97 @@
 # print(character.find(character[-1]))
 # print(character_length)
 
-fruits = ["apple", "banana"]
-fruits.insert(2, "orange")
-fruits.remove("watermelon")
-print(fruits)  
+# fruits = ["apple", "banana"]
+# fruits.insert(2, "orange")
+# fruits.remove("watermelon")
+# print(fruits)  
 # Output: ['apple', 'orange', 'banana']
+
+#  Calculator
+# def add(num1, num2):
+#     return num1 + num2
+
+# def sub(num1, num2):
+#     return num1 - num2
+
+# def mul(num1, num2):
+#     return num1 * num2
+
+# def divide(num1, num2):
+#     if num2 != 0:
+#         return num1 / num2
+#     else:
+#         return "Zero division not allowed!"
+    
+# def sqrt(num):
+#     return num ** 0.5
+
+# def exp(num1, num2):
+#     return num1 ** num2
+
+
+# def main():
+#     while True:
+#         try:
+#             print("\nWelcome to calculate operations!!!\n===================================================\nEnter the operation you want to perform from the options below:")
+#             print("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square root\n6. Exponential\n0. Exit.")
+            
+#             choice = int(input("Enter the operation to perform: "))
+            
+#             if choice == 0:
+#                 print("Exiting the program...")
+#                 break
+#             elif choice == 1:
+#                 num1 = int(input("Enter the first number: "))
+#                 num2 = int(input("Enter the second number: "))
+#                 print(f"The addition of {num1} + {num2} is {add(num1, num2)}")
+#             elif choice == 2:
+#                 num1 = int(input("Enter the first number: "))
+#                 num2 = int(input("Enter the second number: "))
+#                 print(f"The subtraction of {num1} - {num2} is {sub(num1, num2)}")
+#             elif choice == 3:
+#                 num1 = int(input("Enter the first number: "))
+#                 num2 = int(input("Enter the second number: "))    
+#                 print(f"The multiplication of {num1} * {num2} is {mul(num1, num2)}")
+#             elif choice == 4:
+#                 num1 = int(input("Enter the first number: "))
+#                 num2 = int(input("Enter the second number: "))
+#                 print(f"The division of {num1}/{num2} is {divide(num1, num2)}")
+#             elif choice == 5:
+#                 num1 = int(input("Enter the first number: "))
+#                 num2 = int(input("Enter the second number: "))
+#                 print(f"The square root of {num1} is {sqrt(num1)}")
+#                 print(f"The square root of {num2} is {sqrt(num2)}")
+#             elif choice == 6:
+#                 num1 = int(input("Enter the first number: "))
+#                 num2 = int(input("Enter the second number: "))
+#                 print(f"The Exponent of {num1}^{num2} is {exp(num1, num2)}")
+#             else:
+#                 print("Invalid options. Try again!\n")
+#         except ValueError:
+#             print("Oops! Kindly provide number instead.\n")
+#         except KeyboardInterrupt:
+#             print("\nThe program ended due to keyboard interrupt...\n")
+#             break
+#         except Exception as e:
+#             print("Unexpected error occurred: ", e)
+            
+# main()
+
+# name = "olajideAbioye@gmail.com"
+# username, domain = name.split("@")
+# print(username,"\n",domain) 
+
+#  Email Slicer
+while True:
+    try:
+        user_email = input("Kindly enter your email address: ").lower().strip()
+        
+        if "@" in user_email and "." in user_email.split("@"[1]):
+            username, domain = user_email.split("@") 
+            print(f"Username: {username}")
+            print(f"Domain: {domain}")
+        else:
+            print("Invalid email address. Try Again!")
+    except Exception as e:
+        print(f"Error: {e}")
